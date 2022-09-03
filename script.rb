0f0bc2fc782226f1480ca4842659bdcd3a4af16d
trustpizza <maxelolsson@gmail.com>
2022-09-03 09:55:45 -0500

    Use #ord and #chr to convert letters and symbols

--- script.rb
+++ script.rb
@@ -1,16 +1,23 @@
-require 'pry-byebug'
-def cipher(string, shift)
-    alphabet = ('a'..'z').to_a
-    array = string.split('').map { |letter| alphabet.index(letter) - shift}
-    
-    new_array = array.map { |number| number.to_i.to_s(27).tr('0-9a-q', 'a-z')}
+require "pry-byebug"
+def caesar_cipher(string, shifter)
+    letter_to_number = string.split('').map do |letter|
+        if (letter.ord >= 65) and (letter.ord <= 90) # Letters are between A and Z
+            if (letter.ord - shifter) < 65 # Loop back to Z
+                90 - shifter + 1
+            else letter.ord - shifter
+            end
+        elsif (letter.ord >= 97) and (letter.ord <= 122) # Letters are between a and z
+            if (letter.ord - shifter) < 97 # Loop back to z
+                122 - shifter + 1
+            else letter.ord - shifter
+            end
+        else letter
+        end
+    end
+    number_to_letter = letter_to_number.map do |number|
+        number.chr
+    end
+    return_string = number_to_letter.join
 end
 
-cipher('hello', 1)
-# number.to_i.to_s(27).tr('0-9a-q', 'a-z')
-# This cipher maps each letter to a number and then shifts it X numbers over
-# Convert each number to a letter
-# Unsplit the string array
-# Say the main word
-
-#Problems: Does not wrap from A to Z
+caesar_cipher('ab c',1)
